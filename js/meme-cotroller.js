5c560fe824ddb65e180395ac989d499d7ee7c6a7
Gal Ben David <gal.benda3@gmail.com>
2024-09-25 17:48:35 +0300

    Add more tools for adjusting font-size & adding lines and switching between them

--- js/meme-cotroller.js
+++ js/meme-cotroller.js
@@ -2,12 +2,13 @@
 
 let gCanvas
 let gContext
-let gText
 
 function onInit() {
     gCanvas = document.querySelector('canvas')
     gContext = gCanvas.getContext('2d')
 
+    setLineTxt('Add Text Here')
+    setLineDiffPos()
     setImagesInArray()
     renderGallery()
     renderMeme()
@@ -17,44 +18,44 @@ function onInit() {
 function renderMeme() {
     const meme = getMeme()
 
-    gText = meme.lines[0].txt
-
     const img = new Image()
     const imgIdx = meme.selectedImgId
     img.src = getImgUrlById(imgIdx)
 
     img.onload = () => {
         gContext.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
-        createTextLine()
+
+        meme.lines.forEach(line => createTextLine(line))
+        //createTextLine(meme)
     }
 }
 
-function createTextLine() {
+function createTextLine(line) {
     gContext.lineWidth = 3
-    gContext.strokeStyle = 'white'
+    gContext.strokeStyle = line.color
 
     const rectWidth = 300
     const rectHeight = 60
-    const x = (gCanvas.width / 2) - (rectWidth / 2)
-    const y = (gCanvas.height / 2) - (rectHeight / 2) - 200
+    // const x = (gCanvas.width / 2) - (rectWidth / 2)
+    // const y = (gCanvas.height / 2) - (rectHeight / 2) + line.diffPos
 
-    gContext.rect(x, y, rectWidth, rectHeight)
-    gContext.stroke()
+    //gContext.strokeRect(x, y, rectWidth, rectHeight)
 
-    setTextInLine(x, y, rectWidth, rectHeight)
+    setTextInLine(gCanvas.width / 2, gCanvas.height / 2, line)
 }
 
-function setTextInLine(x, y, rectWidth, rectHeight) {
-    gContext.font = '20px Arial'
-    gContext.fillStyle = document.querySelector('.color').value
+function setTextInLine(x, y, line) {
+    gContext.font = `bold ${line.size}px Arial`
+    gContext.fillStyle = line.color
 
-    const text = gText
+    const text = line.txt.toUpperCase()
     const textWidth = gContext.measureText(text).width
-    const textX = x + (rectWidth / 2) - (textWidth / 2)
-    const textY = y + (rectHeight / 2) + 10
+    const textX = x - (textWidth / 2)
+    const textY = y + line.diffPos
 
     gContext.fillText(text, textX, textY)
 }
+
 
 function onGetText(elText) {
     setLineTxt(elText)
@@ -64,6 +65,56 @@ function onGetText(elText) {
 function onImgSelect(imgIdx = 1) {
     getImgUrlById(imgIdx)
     renderMeme()
+}
+
+function onChangeTextSize(elDiff) {
+    changeTextSize(elDiff)
+    renderMeme()
+}
+
+function onChangeTextColor() {
+    const elColor = document.querySelector('.txt-color').value
+    changeTextColor(elColor)
+    renderMeme()
+}
+
+function onAddTextLine() {
+    addTextLine()
+    renderMeme()
+}
+
+function onSwitchTextLine() {
+    const meme = getMeme()
+    const currLineIdx = switchTextLine()
+    document.querySelector('.text').value = meme.lines[currLineIdx].txt
+
+    drawFrame()
+}
+
+function drawFrame() {
+    const meme = getMeme()
+    const currLineIdx = meme.selectedLineIdx
+
+    const rectWidth = 300
+    const rectHeight = 60
+    const x = (gCanvas.width / 2) - (rectWidth / 2)
+    const y = (gCanvas.height / 2) - (rectHeight / 2) + meme.lines[currLineIdx].diffPos
+
+    gContext.clearRect(x - 5, y - 5, rectWidth + 10, rectHeight + 10)
+
+    const img = new Image()
+    const imgIdx = meme.selectedImgId
+    img.src = getImgUrlById(imgIdx)
+
+    img.onload = () => {
+        gContext.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+
+        meme.lines.forEach(line => createTextLine(line))
+
+        gContext.strokeStyle = 'white'
+        gContext.lineWidth = 3
+        gContext.strokeRect(x, y, rectWidth, rectHeight)
+    }
 }
 
 function onDownloadImg(elLink) {
